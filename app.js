3f3109da4f4d4329e2d1fd6284c7e6779af1f068
Aaraav <aaraav2810@gmail.com>
2023-12-15 19:49:36 +0530

    should work

--- app.js
+++ app.js
@@ -12,6 +12,10 @@ require('dotenv').config();
 const port=process.env.PORT || 5000;
 
 
+
+
+
+
 const app = express();
 
 // view engine setup
--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const plm = require("passport-local-mongoose");
 
 mongoose.connect("mongodb://127.0.0.1:27017/pinterest");
-
+mongoose.connect(process.env.MONGO_URL);
 
 const userSchema = new mongoose.Schema({
   username:{ 
